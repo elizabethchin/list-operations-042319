9f8d1e2120113c47016d793412baba6b3ea956cb
Fellowship Student <hackbright@hackbrightacademy.com>
2019-04-23 19:58:15 -0700

    Modified function 1 head

---
+++ list_operations.py
@@ -0,0 +1,272 @@
+"""Functions that manipulate lists without using Python's built-in list methods.
+
+The fundamental operations on lists in Python are those that are part of the
+language syntax and/or cannot be implemented in terms of other list operations.
+They include:
+
+    * List indexing (some_list[index])
+    * List indexing assignment (some_list[index] = value)
+    * List slicing (some_list[start:end])
+    * List slicing assignment (some_list[start:end] = another_list)
+    * List index deletion (del some_list[index])
+    * List slicing deletion (del some_list[start:end])
+
+Implement functions that each use just one of the above operations.
+
+The docstring of each function describes what it should do.
+
+DO NOT USE ANY OF THE BUILT IN LIST METHODS, OR len()!
+"""
+
+
+def head(input_list):
+    """Return the first element of the input list.
+
+    For example:
+
+    >>> head(['Jan', 'Feb', 'Mar'])
+    'Jan'
+
+    """
+
+    return input_list[0]
+
+
+def tail(input_list):
+    """Return all elements of the input list except the first.
+
+    For example:
+
+    >>> tail(['Jan', 'Feb', 'Mar'])
+    ['Feb', 'Mar']
+
+    """
+
+    return []
+
+
+def last(input_list):
+    """Return the last element of the input list.
+
+    For example:
+
+    >>> last(['Jan', 'Feb', 'Mar'])
+    'Mar'
+
+    """
+
+    return []
+
+
+def init(input_list):
+    """Return all elements of the input list except the last.
+
+    For example:
+
+    >>> init(['Jan', 'Feb', 'Mar'])
+    ['Jan', 'Feb']
+
+    """
+
+    return []
+
+
+##############################################################################
+# Do yourself a favor and get a short code review here.
+
+def first_three(input_list):
+    """Return the first three elements of the input list.
+
+    For example:
+
+    >>> first_three(['Jan', 'Feb', 'Mar', 'Apr', 'May'])
+    ['Jan', 'Feb', 'Mar']
+
+    """
+
+    return []
+
+
+def last_five(input_list):
+    """Return the last five elements of the input list.
+
+    For example:
+
+    >>> last_five([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [15, 18, 21, 24, 27]
+
+    """
+
+    return []
+
+
+def middle(input_list):
+    """Return all elements of input_list except the first two and the last two.
+
+    For example:
+
+    >>> middle([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [6, 9, 12, 15, 18, 21]
+
+    """
+
+    return []
+
+
+def inner_four(input_list):
+    """Return the third, fourth, fifth, and sixth elements of input_list.
+
+    For example:
+
+    >>> inner_four([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [6, 9, 12, 15]
+
+    """
+
+    return []
+
+
+def inner_four_end(input_list):
+    """Return the elements that are 6th, 5th, 4th, and 3rd from the end of input_list.
+
+    This function should return those elements in a list, in the exact order
+    described above.
+
+    For example:
+
+    >>> inner_four_end([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
+    [12, 15, 18, 21]
+
+    """
+
+    return []
+
+
+def replace_head(input_list):
+    """Replace the head of input_list with the value 42 and return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_head(multiples)
+    >>> multiples == [42, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    True
+
+    """
+
+    pass
+
+
+def replace_third_and_last(input_list):
+    """Replace third and last elements of input_list with 37 and return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_third_and_last(multiples)
+    >>> multiples == [0, 3, 37, 9, 12, 15, 18, 21, 24, 37]
+    True
+
+    """
+
+    pass
+
+
+def replace_middle(input_list):
+    """Replace all elements of a list but the first and last two with 42 and 37.""Replace all elements of a list but the first and last two with 42 and 37.
+
+    After the replacement, 42 and 37 should appear in that order in input_list.
+""Replace all elements of a list but the first and last two with 42 and 37.
+
+    After the replacement, 42 and 37 should appear in that order in input_list.
+
+    Return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_middle(multiples)
+    >>> multiples == [0, 3, 42, 37, 24, 27]
+    True
+    Return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_middle(multiples)
+    >>> multiples == [0, 3, 42, 37, 24, 27]
+    True
+
+    After the replacement, 42 and 37 should appear in that order in input_list.
+""Replace all elements of a list but the first and last two with 42 and 37.
+
+    After the replacement, 42 and 37 should appear in that order in input_list.
+
+    Return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_middle(multiples)
+    >>> multiples == [0, 3, 42, 37, 24, 27]
+    True
+    Return nothing.
+
+    For example:
+
+    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+    >>> replace_middle(multiples)
+    >>> multiples == [0, 3, 42, 37, 24, 27]
+    True
+
+    """
+
+    pass
+
+
+def delete_third_and_seventh(input_list):
+    """Remove third and seventh elements of input_list and return nothing.
+
+    For example:
+
+    >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+    >>> delete_third_and_seventh(notes)
+    >>> notes == ['Do', 'Re', 'Fa', 'So', 'La', 'Do']
+    True
+
+    """
+
+    pass
+
+
+def delete_middle(input_list):
+    """Remove all elements from input_list except the first two and last two.
+
+    Return nothing.
+
+    For example:
+
+    >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+    >>> delete_middle(notes)
+    >>> notes == ['Do', 'Re', 'Ti', 'Do']
+    True
+
+    """
+
+    pass
+
+
+##############################################################################
+# END OF MAIN EXERCISE.  Yay!  You did it! You Rock!
+#
+# Please ask for a code review from an Education team member before proceeding.
+##############################################################################
+
+# This is the part were we actually run the doctests.
+
+if __name__ == "__main__":
+    import doctest
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("ALL TESTS PASSED")
